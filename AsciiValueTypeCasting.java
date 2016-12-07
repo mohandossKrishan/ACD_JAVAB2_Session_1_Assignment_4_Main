78b75fa4cf5e50e8ac9373ef46f457953b91b1d0
mohandossKrishan <mohandoss28@gmail.com>
2016-12-07 15:57:56 +0530

    AsciiValueTypeCasting

---
+++ AsciiValueTypeCasting.java
@@ -0,0 +1,15 @@
+public class AsciiValueTypeCasting
+{
+    public static void main(String args[])
+    {
+	String ch;
+        int i;
+        for(i=65; i<=90; i++)
+        {
+            ch = new Character((char)i).toString();
+            System.out.println(i+ "-" + ch );
+	    System.out.println(""); 
+        }
+		
+    }
+}
